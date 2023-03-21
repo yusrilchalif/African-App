918733b6aa7b2f2af4ab91ca5b8d27d93345438a
Yusril <yusrilchalif@gmail.com>
2023-03-21 09:52:30 +0700

    Add files via upload
    init

---
+++ African App/African_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  African_AppApp.swift
+//  African App
+//
+//  Created by Yusril on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct African_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
